1a28321aed9af967898d337ffb0c44891ebef041
Akhil Naidu <akhilnaid1997@gmail.com>
2026-01-02 18:36:09 +0530

    roboshop-eks

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -47,19 +47,19 @@ module "eks" {
       desired_size = 2
     }
 
-    green = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-      iam_role_additional_policies = {
-        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazoneEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        }
+    # green = {
+    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+    #   ami_type       = "AL2023_x86_64_STANDARD"
+    #   instance_types = ["m5.xlarge"]
+    #   iam_role_additional_policies = {
+    #     AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    #     AmazoneEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+    #     }
 
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-    }
+    #   min_size     = 2
+    #   max_size     = 10
+    #   desired_size = 2
+    # }
   }
 
   tags = {
